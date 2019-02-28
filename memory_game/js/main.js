0b551496890dc0176261690faa4e5fde4b075066
Trigotometry <tom.hexton@gmail.com>
2019-02-28 15:31:16 +1100

    refined game logic with use of an object array.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,45 @@
-// console.log("Up and running!");
+// console.log('Up and running!');
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [{
+        rank: 'Queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-heats.png'
+    },
+    {
+        rank: 'Queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'King',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'King',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-hearts.png'
+    }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        console.log('You found a match!');
     } else {
-        console.log("Sorry, try again.");
+        console.log('Sorry, try again.');
     }
 };
 
 var flipCard = function(cardId) {
-    console.log("User flipped a " + cards[cardId] + ".");
-    cardsInPlay.push(cards[cardId]);
-	checkForMatch();
+    console.log('User flipped a ' + cards[cardId].rank + ' of ' + cards[cardId].suit + '.');
+    console.log('Image can be found here; ' + cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
+
+    checkForMatch();
 };
 
 flipCard(0);
 flipCard(1);
+
